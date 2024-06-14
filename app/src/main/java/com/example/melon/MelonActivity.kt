2be01e1b701f84efe8982ff522840127c6d0ed97
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-06-14 21:12:20 +0900

    feat: MelonItemRecyclerAdapter 추가

--- app/src/main/java/com/example/melon/MelonActivity.kt
+++ app/src/main/java/com/example/melon/MelonActivity.kt
@@ -1,11 +1,17 @@
 package com.example.melon
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -34,11 +40,37 @@ class MelonActivity : AppCompatActivity() {
                 call: Call<ArrayList<MelonItem>>,
                 response: Response<ArrayList<MelonItem>>
             ) {
-
+                if(response.isSuccessful) {
+                    val melonItemList = response.body()
+                    Log.d("melon", "성공")
+                }
             }
             override fun onFailure(p0: Call<ArrayList<MelonItem>>, p1: Throwable) {
                 Log.d("melon", "요청 실패")
             }
         })
+    }
+}
+
+class MelonItemRecyclerAdapter(
+    val melonItemList: ArrayList<MelonItem>
+    val inflater: LayoutInflater,
+    val glid: RequestManager,
+    val context: Context
+): RecyclerView.Adapter<MelonItemRecyclerAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 }
