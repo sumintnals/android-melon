e499886798151188bf50ef2d0cf1459f1872721b
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-06-14 18:15:30 +0900

    feat: Activity 추가

---
+++ app/src/main/java/com/example/melon/MelonActivity.kt
@@ -0,0 +1,44 @@
+package com.example.melon
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MelonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_melon)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://mellowcode.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val retrofitService = retrofit.create(RetrofitService::class.java)
+
+        retrofitService.getMelonItemList().enqueue(object : Callback<ArrayList<MelonItem>> {
+            override fun onResponse(
+                call: Call<ArrayList<MelonItem>>,
+                response: Response<ArrayList<MelonItem>>
+            ) {
+
+            }
+            override fun onFailure(p0: Call<ArrayList<MelonItem>>, p1: Throwable) {
+                Log.d("melon", "요청 실패")
+            }
+        })
+    }
+}
